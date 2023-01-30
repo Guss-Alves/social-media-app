27ab819a6067e71e03133a5389f3747091f226d7
Guss-Alves <gualvesmagalhaes2020@gmail.com>
2023-01-30 14:22:41 -0800

    login page

---
+++ client/src/pages/login/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './login.css'
+
+const Login = () => {
+    return (
+        <div className='login'>
+            here gonna be login !!!
+        </div>
+    );
+};
+
+
+export default Login;
